c3703f6857c6da2d932cabcc3044b45326ca806a
Jeff Soriano <soriano.jeffm@gmail.com>
2024-03-21 11:49:14 -0700

    Add icons

--- src/App.js
+++ src/App.js
@@ -13,5 +13,9 @@ export default function App() {
       .catch((error) => console.error(error))
   }, [])
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <h1 className="text-3xl font-bold">
+      &#9042; &#8592; &#8593; &#8594; &#8595; &#9733; &#9990; &#10084;
+    </h1>
+  )
 }
