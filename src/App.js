1368b8f8367ac2c0e55da4a7545c0bf5e2e709ee
Jeff Soriano <soriano.jeffm@gmail.com>
2024-03-21 11:05:23 -0700

    Add sample api calls

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import { useEffect } from 'react'
+
 export default function App() {
+  useEffect(() => {
+    fetch('api/hotels/index.json')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error))
+
+    fetch('api/hotels/venetian.json')
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error))
+  }, [])
+
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
