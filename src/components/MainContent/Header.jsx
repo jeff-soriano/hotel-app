88032c42479043872719ac93b9f7c284722437a2
Jeff Soriano <soriano.jeffm@gmail.com>
2024-03-22 14:13:10 -0700

    Small refactors to Header component

--- src/components/MainContent/Header.jsx
+++ src/components/MainContent/Header.jsx
@@ -13,23 +13,14 @@ export default function Header() {
           </div>
         </div>
         <div className="flex items-end">
-          <span>
-            <span>&#9042;</span>
-            <span className="ml-1">Strip</span>
-          </span>
-          <span className="ml-4">
-            <span>&#9990;</span>
-            <span className="ml-1">1-866-4279</span>
-          </span>
-          <span className="ml-4">
-            <span>&#10084;</span>
-            <span className="ml-1">Best Price Guarantee</span>
-          </span>
+          <span>&#9042; Strip</span>
+          <span className="ml-4">&#9990; 1-866-4279</span>
+          <span className="ml-4">&#10084; Best Price Guarantee</span>
         </div>
       </div>
       <div>
         <div className="text-5xl font-bold text-orange-500">$128</div>
-        <div>HOTEL ROOMS FROM</div>
+        <div className="text-gray-600">HOTEL ROOMS FROM</div>
       </div>
     </div>
   )
